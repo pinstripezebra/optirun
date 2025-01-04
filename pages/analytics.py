be4fb4053d2fd6e8b76617005012a8b81a60e1a8
pinstripezebra <seelcs12@gmail.com>
2025-01-04 10:21:46 -0800

    updating home page to use data from dcc.store

--- pages/analytics.py
+++ pages/analytics.py
@@ -13,73 +13,70 @@ from utility.chatbot import query_condition_description
 
 dash.register_page(__name__, path='/analytic')
 
-# Note will need to pass these in from app
-df1 = pd.read_csv("Data/weather_data.csv")
-df1['time'] = pd.to_datetime(df1['time'])
-
 # loading environmental variables
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 api_key = os.getenv("ANTHROPIC_API_KEY")
-optimal_conditions = {'temperature_2m': float(os.getenv("OPTIMAL_TEMP")),
-                      'cloudcover': float(os.getenv("OPTIMAL_CLOUD")),
-                      'windspeed_10m': float(os.getenv("OPTIMAL_WIND"))}
-forecasted_conditions = {'temperature_2m': df1['temperature_2m'].to_list(),
-                         'cloudcover': df1['cloudcover'].to_list(),
-                         'windspeed_10m': df1['windspeed_10m'].to_list()}
-
-# Rating weather conditions
-max_window = len(df1['temperature_2m'].to_list())
-conditions = find_optimal_window(optimal_conditions, forecasted_conditions, max_window)
-
-# Adding forecast to dataframe
-df1['Forecast_Score'] = conditions['Score']
-
-# determining optimal start/stop times
-next_12_hours = df1.head(12)
-best_bucket = next_12_hours[next_12_hours['Forecast_Score'] == next_12_hours['Forecast_Score'].max()]
-start_time = best_bucket['time'].to_list()[0]
-end_time = start_time + timedelta(hours=1)
-
 
 layout = html.Div([
     html.H1('Running Outlook Today'),
-    html.Div([
-    draw_Text(html.P("The best expected running time today is between {start} and {end}.".format(start = start_time, end = end_time))),
-
-    ],style= {'width': '50%'}),
+    html.Div([],style= {'width': '50%'}, id='best-times-forecast'),
     html.H3('Condition Overview'),
     dbc.Row([
-        html.Div([
-            draw_Text(query_condition_description(api_key, [best_bucket['temperature_2m'].to_list()[0],
-                                                        best_bucket['windspeed_10m'].to_list()[0],
-                                                        best_bucket['cloudcover'].to_list()[0]]))
-        ])
+        html.Div([], id = 'forecast-figure')
     ]),
     html.Br(),
 
-   # Generates a graph of the forecast
-    html.Div([
-        dbc.Row([
-            dbc.Col([
-                    draw_Image(generate_run_plot(df1, 'Forecast_Score')), 
-                ],  
-            width={"size": 6, "offset": 0}),
-            dbc.Col([
-                    dbc.Row([
-                        draw_Image(generate_gauge_plot(df1, 'temperature_2m'), 200)
-                    ]),
-                    dbc.Row([
-                        draw_Image(generate_gauge_plot(df1, 'cloudcover'), 200)
-                    ]),
-                    dbc.Row([
-                        draw_Image(generate_gauge_plot(df1, 'windspeed_10m'), 200)
-                    ]),
-
-                 ]) 
-        ])
-            
-    ])
+   # Generates gauges of forecast
+    html.Div([], id = 'running-gauges')
 ])
 
+
+# callback for running gauges
+@callback(
+    Output(component_id='running-gauges', component_property='children'),
+    Output(component_id='best-times-forecast', component_property='children'),
+    Output(component_id='forecast-figure', component_property='children'),
+    Input('stored-forecast', 'data'),
+    Input('location-storage', 'data')
+)
+def update_timeseries(df, location):
+
+    # reading in dataframe from store
+    filtered_df = pd.read_json(df, orient='split')
+    filtered_df['time'] = pd.to_datetime(filtered_df['time'])
+
+    # determining optimal start/stop times
+    next_12_hours = filtered_df.head(12)
+    best_bucket = next_12_hours[next_12_hours['Forecast_Score'] == next_12_hours['Forecast_Score'].max()]
+    start_time = best_bucket['time'].to_list()[0]
+    end_time = start_time + timedelta(hours=1)
+
+    # best forecast return
+    best_forecast = draw_Text(html.P("The best expected running time today is between {start} and {end}.".format(start = start_time, end = end_time))),
+
+    # gauges return
+    gauges = dbc.Row([
+                dbc.Col([
+                        draw_Image(generate_run_plot(filtered_df, 'Forecast_Score')), 
+                    ],  
+                width={"size": 6, "offset": 0}),
+                dbc.Col([
+                        dbc.Row([
+                            draw_Image(generate_gauge_plot(filtered_df, 'temperature_2m'), 200)
+                        ]),
+                        dbc.Row([
+                            draw_Image(generate_gauge_plot(filtered_df, 'cloudcover'), 200)
+                        ]),
+                        dbc.Row([
+                            draw_Image(generate_gauge_plot(filtered_df, 'windspeed_10m'), 200)
+                        ]),
+                    ]) 
+            ])
+    
+    # forecast return
+    forecast_return = draw_Text(query_condition_description(api_key, [best_bucket['temperature_2m'].to_list()[0],
+                                                        best_bucket['windspeed_10m'].to_list()[0],
+                                                        best_bucket['cloudcover'].to_list()[0]]))
+    return  best_forecast, gauges, forecast_return
 
